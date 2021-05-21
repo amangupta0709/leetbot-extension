b80f3ff7ce219821b3d6f357b6eb6f700ef4f682
amangupta0709 <guptaaman704@gmail.com>
2021-05-21 17:44:19 +0530

    changed default names and codes

--- src/options/Options.tsx
+++ src/options/Options.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from '../logo.svg';
-import './Options.css';
+import './Options.css'
 
 function Options() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/options/Options.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <p>This is options page!!.</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default Options;
+export default Options
